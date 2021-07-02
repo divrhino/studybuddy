57ddf450e9cce900499046ce8e1707eddd0f83a6
divrhino <fancydresspenguin+divrhino@gmail.com>
2021-07-02 10:33:24 +0800

    Add promptGetInput function

--- cmd/new.go
+++ cmd/new.go
@@ -16,8 +16,11 @@ limitations under the License.
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -38,4 +41,36 @@ type promptContent struct {
 
 func init() {
 	noteCmd.AddCommand(newCmd)
+}
+
+func promptGetInput(pc promptContent) string {
+	validate := func(input string) error {
+		if len(input) <= 0 {
+			return errors.New(pc.errorMsg)
+		}
+		return nil
+	}
+
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{ . }} ",
+		Valid:   "{{ . | green }} ",
+		Invalid: "{{ . | red }} ",
+		Success: "{{ . | bold }} ",
+	}
+
+	prompt := promptui.Prompt{
+		Label:     pc.label,
+		Templates: templates,
+		Validate:  validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input: %s\n", result)
+
+	return result
 }
