8a43399ee1c65383fd5ba07163ea4b8d95e2ecc8
divrhino <fancydresspenguin+divrhino@gmail.com>
2021-07-02 10:21:44 +0800

    Create db table function

--- data/data.go
+++ data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -17,4 +18,21 @@ func OpenDatabase() error {
 	}
 
 	return db.Ping()
+}
+
+func CreateTable() {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS studybuddy (
+		"idNote" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"word" TEXT,
+		"definition" TEXT,
+		"category" TEXT
+	  );`
+
+	statement, err := db.Prepare(createTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	statement.Exec()
+	log.Println("Studybuddy table created")
 }
